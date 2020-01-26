b68ab93b6f89dedb922dfbcc1b689415738dad7a
dionysus11 <52480056+dionysus11-source@users.noreply.github.com>
2020-01-26 10:07:16 +0900

    add save csv function

--- ryzaSearch.py
+++ ryzaSearch.py
@@ -1,25 +1,42 @@
-
 import requests
 from bs4 import BeautifulSoup
+import os.path
+import pandas as pd
 
-def searchKeyword(keyword):
+filename = 'ryza.csv'
+def downloadCsv():
     header = {"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"}
     url = 'https://bbs.ruliweb.com/game/85195/read/9418448'
     r = requests.get(url, headers=header)
     bs = BeautifulSoup(r.content,'lxml')
-    # keyword = '깨끗한 물'
-    ret = keyword + '\n'
-    divs = bs.select('tr')
+    divs = bs.select('#board_read > div > div.board_main > div.board_main_view > div.view_content.autolink > div > table > tbody > tr')
+    item = []
+    location = []
+    info = []
+    col = divs[0].text.split('\n')[1:4]
+    col1, col2, col3 = tuple(col)
+    del divs[0]
+    for i in divs:
+        a = i.text.split('\n')[1:4]
+        item.append(a[0])
+        location.append(a[1])
+        info.append(a[2])
+    data = {col1:item, col2:location, col3:info}
+    df = pd.DataFrame(data)
+    df.to_csv(filename)
+
+#if not os.exist(filename):
+def searchKeyword(keyword):
+    if not os.path.exists(filename):
+        downloadCsv()
+    df = pd.read_csv(filename)
     cnt = 1
-    for div in divs:
-        tds = div.select('td')
-        if len(tds) == 3:
-            items = tds[0]
-            locations = tds[1]
-            descs = tds[2]
-            if keyword in items.text:
-                ret += str(cnt) + ') '+ items.text + ': 장소:'  +  locations.text + '\n특징: ' + descs.text + '\n'
-                cnt += 1
+    ret = keyword + '\n'
+    for i in df.index:
+        target = df.at[i, '소재']
+        if keyword in target:
+            ret += str(cnt) + ') '+ target + ': 장소:'  +  df.at[i, '입수 장소'] + '\n특징: ' + df.at[i, '자세한 사항'] + '\n'
+            cnt += 1
     if cnt == 1:
         return 'Not found'
     else:
